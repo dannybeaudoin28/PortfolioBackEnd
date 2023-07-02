18c1c56446df24ed00201e4800cd3850c3d847f5
Danny B <danny.beaudoin@student.sl.on.ca>
2023-07-02 19:18:51 -0400

    Initial Commit for back end

---
+++ src/main/java/dbportfolio/backend/Controllers/CourseController.java
@@ -0,0 +1,10 @@
+package dbportfolio.backend.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin
+public class CourseController {
+    
+}
