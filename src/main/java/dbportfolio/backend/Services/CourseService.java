cbf973babdb733c53b4f451490f8599d5ca4c8c3
Danny B <danny.beaudoin@student.sl.on.ca>
2023-07-04 11:26:18 -0400

    Add service layer methods for manipulating course objects

--- src/main/java/dbportfolio/backend/Services/CourseService.java
+++ src/main/java/dbportfolio/backend/Services/CourseService.java
@@ -1,5 +1,31 @@
 package dbportfolio.backend.Services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dbportfolio.backend.Models.Course;
+import dbportfolio.backend.Repositories.CourseRepo;
+
+@Service
 public class CourseService {
-    
+    @Autowired
+    private CourseRepo courseRepo;
+
+    public Integer addCourse(Course course) {
+        return this.courseRepo.save(course).getId();
+    }
+
+    public Course getCourseById(Integer id) {
+        return this.courseRepo.findById(id).get();
+    }
+
+    public ArrayList<Course> getAllCourses() {
+        return (ArrayList<Course>) this.courseRepo.findAll();
+    }
+
+    public void deleteCourseById(Integer id) {
+        this.courseRepo.deleteById(id);
+    }
 }
